80cb2ad1e5b7d7c1fa883890849c48fc079c3818
tsuji <ltksk0714@gmail.com>
2018-10-17 23:30:40 +0900

    [modify] codeを整理

--- handgun_config.py
+++ handgun_config.py
@@ -2,7 +2,9 @@
 import os
 import yaml
 
+
 _CONFIG_DATA = None
+
 
 config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                            "config",
@@ -10,14 +12,18 @@ config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
 with open(config_path) as config_data:
     _CONFIG_DATA = yaml.load(config_data)
 
+
 def mongo_host():
     return _CONFIG_DATA["MONGODB"]["HOST"]
+
 
 def mongo_port():
     return int(_CONFIG_DATA["MONGODB"]["PORT"])
 
+
 def debug_mode():
     return _CONFIG_DATA["APPLICATION"]["DEBUG_MODE"]
+
 
 def jwt_secret_key():
     return _CONFIG_DATA["APPLICATION"]["AUTHORIZATION"]["SECRET_KEY"]
